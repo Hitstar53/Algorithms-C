addf1bda34f04faf4fd22878b90da381811c640c
Hatim Sawai <hatimsawai76@gmail.com>
2023-04-03 05:21:53 +0530

    exp 5
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Exp5/exp5.c
+++ Exp5/exp5.c
@@ -13,8 +13,8 @@ void generate_activities(struct activity *activity, int n)
     srand(time(0));
     for (int i = 0; i < n; i++)
     {
-        activity[i].start = rand() % 1000;
-        activity[i].end = rand() % 1000 + activity[i].start + 1;
+        activity[i].start = rand()%100;
+        activity[i].end = rand()%100 + activity[i].start + 1;
         activity[i].completion = activity[i].end - activity[i].start;
     }
 }
@@ -106,9 +106,20 @@ int main(int argc, char const *argv[])
     printf("Activities generated:\n");
     print_activities(activity, n);
     sort_activities(activity, n);
-    printf("\nActivities sorted:\n");
-    print_activities(activity, n);
+    // printf("\nActivities sorted:\n");
+    // print_activities(activity, n);
+    //time taken by greedy approach
+    clock_t start, end;
+    start = clock();
     activity_selector_greedy(activity, n);
+    end = clock();
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nTime taken by greedy approach: %f seconds\n", time_taken);
+    //time taken by dynamic programming
+    start = clock();
     activity_selector_dynamic(activity, n);
+    end = clock();
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nTime taken by dynamic programming: %f seconds\n", time_taken);
     return 0;
 }
